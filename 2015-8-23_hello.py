307a2bd650233ad6ee29dee5fc0236bb0f1f8d00
root <hugesoft@126.com>
2015-08-24 00:18:55 +0800

    修改：     requirements.txt

--- 2015-8-23_hello.py
+++ 2015-8-23_hello.py
@@ -3,7 +3,6 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-import os
 from flask.ext.script import Manager
 from flask import Flask,render_template,session,redirect,url_for,flash
 from flask.ext.bootstrap import Bootstrap
@@ -12,17 +11,8 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 app =Flask(__name__)
-#数据库的配置
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
-
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 app.debug = True
@@ -33,24 +23,6 @@ moment = Moment(app)
 class NameForm(Form):
     name = StringField(u'请输入你的姓名', validators=[Required()])
     submit = SubmitField(u'提交')
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
